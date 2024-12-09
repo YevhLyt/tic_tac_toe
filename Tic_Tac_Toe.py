080a0da3f13f32a5a7b8e8611e79fe4b769f61c9
Hunter1995 <zenyalyt@gmail.com>
2024-12-09 14:31:31 +0200

    import pygame and creating board

---
+++ Tic_Tac_Toe.py
@@ -0,0 +1,10 @@
+import pygame
+#create the screen
+pygame.init()
+screen = pygame.display.set_mode((320,240))
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+HelloWorldColors = [BLACK, RED]
+font = pygame.font.Font(None, 36)
+count = 0
